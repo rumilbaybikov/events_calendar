b0395aceaadec6ee799cabfdb3f02792fcf8e666
Rumil <rumil.baybikov@gmail.com>
2012-08-28 12:37:52 +0400

    mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,11 @@ EventsCalendar::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'fast-beyond-4575.herokuapp.com' }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,6 @@
+ActionMailer::Base.smtp_settings = {
+    :address   => "smtp.gmail.com",
+    :port      => 587,
+    :user_name => "rumil.baybikov@gmail.com",
+    :password  => "killerbean"
+}
